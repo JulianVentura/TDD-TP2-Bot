22e3724056e8b5896a80ee0de73448061a54bf4e
AgustinLeguizamon <agustinmleguizamon@gmail.com>
2021-12-04 13:07:55 -0300

    agregado error ErrorParseo a comando /realizar_oferta

--- app/routes.rb
+++ app/routes.rb
@@ -114,6 +114,8 @@ class Routes
 
     bot.api.send_message(chat_id: message.chat.id,
                          text: "Has realizado con exito la oferta numero #{respuesta.id_oferta} al auto de patente #{respuesta.patente} con un precio de #{respuesta.precio}")
+  rescue ErrorParseo
+    bot.api.send_message(chat_id: message.chat.id, text: 'Error: El uso del comando es /realizar_oferta <patente>, <precio_oferta>')
   rescue ErrorApi => e
     bot.api.send_message(chat_id: message.chat.id, text: e.mensaje)
   end
